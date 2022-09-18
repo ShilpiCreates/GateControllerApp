8af7123e1d4462bc69930295d1fa6960ba9d6b25
ShilpiCreates <shilpi.creates@gmail.com>
2022-09-18 23:38:00 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -36,9 +36,50 @@ document.querySelector('.allModel').onclick = () =>{
     }
 }
 
-//----------Notification----------//
-const newNotify = document.querySelector('.newNotifi');
-
-document.querySelector('.notification').onclick = () =>{
-    newNotify.classList.toggle('active');
+document.querySelector('.model-list li').onclick = () =>{
+    model_list.classList.remove('active');
+    if(model_list.classList.contains('active')){
+        document.querySelector('.allModel').classList.add('active');
+    }else{
+        document.querySelector('.allModel').classList.remove('active');
+    }
 }
+//----------Notification----------//
+$('.notification').on('click', function(){
+    $('.newNotifi').toggle('active');
+});
+
+//----------Card Popup----------//
+
+$(".card, #CheckOut").on('click', function() {
+    $(".main-popup").addClass('active');
+  }); 
+  $(".close").click(function(){
+    $(".main-popup").removeClass('active');
+  });
+
+  
+$(".btn").on('click', function() {
+    $(".VehicleCard_list").addClass('active');
+  }); 
+
+
+//----------Error Popup----------//
+$(".search-btn").on('click', function() {
+    $(".error-container").addClass('active');
+  }); 
+  $(".btn-line").click(function(){
+    $(".error-container").removeClass('active');
+  });
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
